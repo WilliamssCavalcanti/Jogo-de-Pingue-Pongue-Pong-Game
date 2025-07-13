b20561db92a10cad656fd4e14f1e1e6f70924b3b
William C <166062273+WilliamssCavalcanti@users.noreply.github.com>
2025-07-13 20:02:18 -0300

    Update main.py
    Adicionei comentários para ajudar na leitura do código

--- main.py
+++ main.py
@@ -4,30 +4,31 @@ from ball import Ball
 from scoreboard import Scoreboard
 import time
 
+# Configuração da jnaela do jogo
 screen = Screen()
-screen.bgcolor('black')
-screen.setup(width=800, height=600)
-screen.title('Meu Jogo de Pingue-Pongue / My Pong Game')
-screen.tracer(0)
+screen.bgcolor('black') # Cor do fundo definida como preto
+screen.setup(width=800, height=600) # Tamanho da tela 
+screen.title('Meu Jogo de Pingue-Pongue / My Pong Game') # Titulo da janela
+screen.tracer(0) # Desativa a atualização automática da tela
 
-r_paddle = Paddle((350, 0))
-l_paddle = Paddle((-350, 0))
-ball = Ball()
-scoreboard = Scoreboard()
+r_paddle = Paddle((350, 0)) # "Barra"(raquete) da direita na posição x = 350
+l_paddle = Paddle((-350, 0)) # "Barra"(raquete) da esquerda na posição x = -350
+ball = Ball() # Bola
+scoreboard = Scoreboard() # Placar
 
-screen.listen()
-screen.onkey(r_paddle.go_up, 'Up')
-screen.onkey(r_paddle.go_down, 'Down')
-screen.onkey(l_paddle.go_up, 'w')
-screen.onkey(l_paddle.go_down, 's')
+screen.listen() # Faz a janela escutar os botões do teclado
+screen.onkey(r_paddle.go_up, 'Up') # Tecla para cima faz a "barra"(Raquete) direita ir para cima
+screen.onkey(r_paddle.go_down, 'Down') # Tecla para baixo faz a "barra"(Raquete) direita ir para baixo
+screen.onkey(l_paddle.go_up, 'w') # Tecla " W " faz a "barra"(Raquete) esquerda ir para cima
+screen.onkey(l_paddle.go_down, 's') # Tecla " S " faz a "barra"(Raquete) esquerda ir para baixo
 
 
 
 game_is_on = True
-while game_is_on:
-    time.sleep(ball.move_speed)
-    screen.update()
-    ball.move()
+while game_is_on: # Loop principal do jogo
+    time.sleep(ball.move_speed) # Controla a velocidade do jogo com base na bola
+    screen.update() # Atualiza a tela manualmente (ja que o tracer está desligado)
+    ball.move() # Move a bola
 
     #Detectar colisão com a parede
     if ball.ycor() > 280 or ball.ycor() < -280:
@@ -40,10 +41,10 @@ while game_is_on:
     #Detectar se a barrinha da direita erra a bola
     if ball.xcor() > 380:
         ball.reset_position()
-        scoreboard.l_point()
+        scoreboard.l_point() # Jogador da esquerda marca ponto
 
     # Detectar se a barrinha da esquerda erra a bola
     if ball.xcor() < -380:
-        ball.reset_position()
+        ball.reset_position() # Jogador da direita marca ponto
     
-screen.exitonclick()
+screen.exitonclick() # Fecha a janela ao clicar
