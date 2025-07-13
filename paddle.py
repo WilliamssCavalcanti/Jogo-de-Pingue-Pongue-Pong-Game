2fe9853550d6551b36268c3ef053e599e2768bec
William C <166062273+WilliamssCavalcanti@users.noreply.github.com>
2025-07-13 19:52:51 -0300

    Update paddle.py
    Adicionei comentários para ajudar na leitura do código

--- paddle.py
+++ paddle.py
@@ -5,16 +5,16 @@ class Paddle(Turtle):
 
     def __init__(self, position):
         super().__init__()
-        self.shape('square')
-        self.color('white')
-        self.shapesize(stretch_wid=5, stretch_len=1)
-        self.penup()
-        self.goto(position)
+        self.shape('square') # Define a forma das "barras"(raquetes) como quadrados
+        self.color('white') # Define a cor das "barras"(raquetes) como brancas
+        self.shapesize(stretch_wid=5, stretch_len=1) # Estica a forma para deixar mais parecida com as "barras"(raquetes) das versões antigas do jogo
+        self.penup() # Impede que as "barras"(raquetes) desenhem ao se movimentar
+        self.goto(position) # Move a raquete para a posição inicial (definida ao criar o objeto)
 
     def go_up(self):
-        new_y = self.ycor() + 20
+        new_y = self.ycor() + 20 # Move a "barra"(raquete) 20 unidades para cima
         self.goto(self.xcor(), new_y)
 
-    def go_down(self):
+    def go_down(self): # Move a "barra"(raquete) 20 unidades para baixo
         new_y = self.ycor() - 20
         self.goto(self.xcor(), new_y)
